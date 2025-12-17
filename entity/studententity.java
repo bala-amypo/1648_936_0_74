0a34d8848c7e5798d9627cbcce80980f72b77e86
bala-amypo <bala@amypo.in>
2025-12-17 06:21:49 +0000

    User commit

--- entity/studententity.java
+++ entity/studententity.java
@@ -2,20 +2,30 @@ package.com.example.demo.entity
 @Entity
 public class studententity{
     private Integer id;
-    private String user;
-    private String email;
-    private String password;
-    private Date date;
-    private Time time;
-}
-public Integer getId() {return id;}
-public void setId(Integer id) {this.id=id;}
+        private String user;
+            private String email;
+                private String password;
+                    private Date date;
+                    }
+                    public Integer getId() {return id;}
+                    public void setId(Integer id) {this.id=id;}
 
-public String getUser() {return user;}
-public void setUser(String user) {this.user=user}
+                    public String getUser() {return user;}
+                    public void setUser(String user) {this.user=user}
 
-public String getEmail() {return email;}
-public void setEmail(String email) {this.email=email}
+                    public String getEmail() {return email;}
+                    public void setEmail(String email) {this.email=email}
 
-public String getPassword() {return password;}
-public void setPassword(String password) {this.password=password}
+                    public String getPassword() {return password;}
+                    public void setPassword(String password) {this.password=password}
+
+                    public Date getUser() {return date;}
+                    public void setUser(Date date) {this.date=date}
+
+                    public void main(Integer id, String user,String email,String password,Date date){
+                        this.id=id;
+                            this.user=user;
+                                this.email.email;
+                                    this.password=password;
+                                        this.date=date;
+                                        }
