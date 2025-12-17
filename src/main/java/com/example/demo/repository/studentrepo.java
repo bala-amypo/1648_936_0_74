5a259df90a8604a1ac0a8f4e75a5d3ef5285be75
bala-amypo <bala@amypo.in>
2025-12-17 10:07:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/studentrepo.java
+++ src/main/java/com/example/demo/repository/studentrepo.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository;
-import org.springframework.sterotype.Repository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRopsitory;
 import com.exampl.demo.entity.studententity;
 @Repository
--- src/main/java/com/example/demo/service/studentservice.java
+++ src/main/java/com/example/demo/service/studentservice.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
 import com.example.demo.entity.studententity;
-import com.example.demo.entity.student;
-
 public interface studentservice{
 studententity postData(studententity stu);
 
