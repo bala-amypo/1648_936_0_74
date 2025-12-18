1808a6c366a9deab10aebab50393a26426f22979
bala-amypo <bala@amypo.in>
2025-12-18 09:50:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/validationcontroller.java
+++ src/main/java/com/example/demo/controller/validationcontroller.java
@@ -14,3 +14,11 @@ public class validationcontroller{
       return va.postData(val);
   }
 }
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.ValidationEntity;
+
+public interface ValidationRepo extends JpaRepository<ValidationEntity,Long>{
+      
+}
