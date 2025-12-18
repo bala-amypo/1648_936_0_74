8af605606fd834ae6b28575ecd0c30f3066c6cbc
bala-amypo <bala@amypo.in>
2025-12-18 10:05:03 +0000

    User commit

--- src/main/java/com/example/demo/service/validationservice.java
+++ src/main/java/com/example/demo/service/validationservice.java
@@ -3,8 +3,4 @@ import com.example.demo.entity.validationentity;
 import java.util.List;
 public interface validationservice{
 validationentity postdata(validationentity id);
-// List<studententity>getAll();
-// String delete(int id);
-// validationentity findid(int id);
-// validationentity put(int id,validationentity entity);
 }
