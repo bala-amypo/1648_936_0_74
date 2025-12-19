14c6dd1ff3b13d019b121e9aa96fa316333741f9
bala-amypo <bala@amypo.in>
2025-12-19 05:48:55 +0000

    User commit

--- src/main/java/com/example/demo/exception/golbalexce.java
+++ src/main/java/com/example/demo/exception/golbalexce.java
@@ -6,7 +6,7 @@ import com.example.demo.exception.validationexce;
 @RestControllerAdvice
 public class gobalexce{
     @ExceptionHandler(validationexce.class)
-    public ResponseEntity<String> handleValidation(validationexce ex){
+    public ResponseEntity<String> handleValidationexeception(validationexce ex){
     return new ResponseEntity<String>("This is error is shown in the UI",Httpstatus.SUCCE);
     }
 }
