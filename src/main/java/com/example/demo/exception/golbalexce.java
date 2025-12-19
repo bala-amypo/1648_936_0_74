17aeeeff07445f7928c705e7cfc3ce4623e04aee
bala-amypo <bala@amypo.in>
2025-12-19 05:53:21 +0000

    User commit

--- src/main/java/com/example/demo/exception/golbalexce.java
+++ src/main/java/com/example/demo/exception/golbalexce.java
@@ -1,12 +1,12 @@
 import com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.exception.validationexce;
 @RestControllerAdvice
 public class gobalexce{
     @ExceptionHandler(validationexce.class)
-    public ResponseEntity<String> handleValidationexeception(validationexce ex){
-    return new ResponseEntity<String>(ex.getMessage(),Httpstatus.SUCCESS);
+    public ResponseBody<String> handleValidationexeception(validationexce ex){
+    return new ResponseBody<String>(ex.getMessage(),Httpstatus.SUCCESS);
     }
 }
