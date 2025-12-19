b40284acd8cb3ecd30205da817f9f707963b9c6f
bala-amypo <bala@amypo.in>
2025-12-19 05:52:16 +0000

    User commit

--- src/main/java/com/example/demo/exception/golbalexce.java
+++ src/main/java/com/example/demo/exception/golbalexce.java
@@ -1,7 +1,7 @@
 import com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseEntity;
 import com.example.demo.exception.validationexce;
 @RestControllerAdvice
 public class gobalexce{
