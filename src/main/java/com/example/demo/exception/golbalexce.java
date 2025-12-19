94c5dfac405955cbda2c08965dfd0efe065c7118
bala-amypo <bala@amypo.in>
2025-12-19 05:32:31 +0000

    User commit

--- src/main/java/com/example/demo/exception/golbalexce.java
+++ src/main/java/com/example/demo/exception/golbalexce.java
@@ -1,8 +1,12 @@
 import com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponceBody;
 
 @RestControllerAdvice
 public class gobalexce{
     @ExceptionHandler(validationexce.class)
+    public ResponceBody<String> handleValidation(){
+        return "This is error is shown in the UI";
+    }
 }
